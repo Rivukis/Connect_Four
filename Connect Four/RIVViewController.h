6db648b5861d24be669479ae35040781f53779f2
Brian Radebaugh <rivukis@gmail.com>
2014-05-05 09:26:54 -0700

    Initial Commit

---
+++ Connect Four/RIVViewController.h
@@ -0,0 +1,13 @@
+//
+//  RIVViewController.h
+//  Connect Four
+//
+//  Created by Brian Radebaugh on 5/5/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RIVViewController : UIViewController
+
+@end
---
+++ Connect FourTests/Connect_FourTests.m
@@ -0,0 +1,34 @@
+//
+//  Connect_FourTests.m
+//  Connect FourTests
+//
+//  Created by Brian Radebaugh on 5/5/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Connect_FourTests : XCTestCase
+
+@end
+
+@implementation Connect_FourTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
